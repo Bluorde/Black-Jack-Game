3442899e934910be4d1171142a2bb609be770183
Bluorde <vermil.elton@gmail.com>
2024-11-28 09:45:38 +0300

    First Commit

---
+++ Backup.js
@@ -0,0 +1,315 @@
+const twoOfClubs = new Image()
+twoOfClubs.src = "./Playing Cards/PNG-cards-1.3/2_of_clubs.png"
+twoOfClubs.width = 200
+twoOfClubs.height = 300
+twoOfClubs.name = "two";
+const twoOfDiamonds = new Image()
+twoOfDiamonds.src = "./Playing Cards/PNG-cards-1.3/2_of_diamonds.png"
+twoOfDiamonds.width = 200
+twoOfDiamonds.height = 300
+twoOfDiamonds.name = "two";
+const twoOfHearts = new Image()
+twoOfHearts.src = "./Playing Cards/PNG-cards-1.3/2_of_hearts.png"
+twoOfHearts.width = 200
+twoOfHearts.height = 300
+twoOfHearts.name = "two";
+const twoOfSpades = new Image()
+twoOfSpades.src = "./Playing Cards/PNG-cards-1.3/2_of_spades.png"
+twoOfSpades.width = 200
+twoOfSpades.height = 300
+twoOfSpades.name = "two"
+const threeOfClubs = new Image()
+threeOfClubs.src = "./Playing Cards/PNG-cards-1.3/3_of_clubs.png"
+threeOfClubs.width = 200
+threeOfClubs.height = 300
+threeOfClubs.name = "three"
+const threeOfDiamonds = new Image()
+threeOfDiamonds.src = "./Playing Cards/PNG-cards-1.3/3_of_diamonds.png"
+threeOfDiamonds.width = 200
+threeOfDiamonds.height = 300
+threeOfDiamonds.name = "three"
+const threeOfHearts = new Image()
+threeOfHearts.src = "./Playing Cards/PNG-cards-1.3/3_of_hearts.png"
+threeOfHearts.width = 200
+threeOfHearts.height = 300
+threeOfHearts.name = "three"
+const threeOfSpades = new Image()
+threeOfSpades.src = "./Playing Cards/PNG-cards-1.3/3_of_spades.png"
+threeOfSpades.width = 200
+threeOfSpades.height = 300
+threeOfSpades.name = "three"
+const fourOfClubs = new Image()
+fourOfClubs.src = "./Playing Cards/PNG-cards-1.3/4_of_clubs.png"
+fourOfClubs.width = 200
+fourOfClubs.height = 300
+fourOfClubs.name = "four"
+const fourOfDiamonds = new Image()
+fourOfDiamonds.src = "./Playing Cards/PNG-cards-1.3/4_of_diamonds.png"
+fourOfDiamonds.width = 200
+fourOfDiamonds.height = 300
+fourOfDiamonds.name = "four"
+const fourOfHearts = new Image()
+fourOfHearts.src = "./Playing Cards/PNG-cards-1.3/4_of_hearts.png"
+fourOfHearts.width = 200
+fourOfHearts.height = 300
+fourOfHearts.name = "four"
+const fourOfSpades = new Image()
+fourOfSpades.src = "./Playing Cards/PNG-cards-1.3/4_of_spades.png"
+fourOfSpades.width = 200
+fourOfSpades.height = 300
+fourOfSpades.name = "four"
+const fiveOfClubs = new Image()
+fiveOfClubs.src = "./Playing Cards/PNG-cards-1.3/5_of_clubs.png"
+fiveOfClubs.width = 200
+fiveOfClubs.height = 300
+fiveOfClubs.name = "five"
+const fiveOfDiamonds = new Image()
+fiveOfDiamonds.src = "./Playing Cards/PNG-cards-1.3/5_of_diamonds.png"
+fiveOfDiamonds.width = 200
+fiveOfDiamonds.height = 300
+fiveOfDiamonds.name = "five"
+const fiveOfHearts = new Image()
+fiveOfHearts.src = "./Playing Cards/PNG-cards-1.3/5_of_hearts.png"
+fiveOfHearts.width = 200
+fiveOfHearts.height = 300
+const fiveOfSpades = new Image()
+fiveOfSpades.src = "./Playing Cards/PNG-cards-1.3/5_of_spades.png"
+fiveOfSpades.width = 200
+fiveOfSpades.height = 300
+const sixOfClubs = new Image()
+sixOfClubs.src = "./Playing Cards/PNG-cards-1.3/6_of_clubs.png"
+sixOfClubs.width = 200
+sixOfClubs.height = 300
+const sixOfDiamonds = new Image()
+sixOfDiamonds.src = "./Playing Cards/PNG-cards-1.3/6_of_diamonds.png"
+sixOfDiamonds.width = 200
+sixOfDiamonds.height = 300
+const sixOfHearts = new Image()
+sixOfHearts.src = "./Playing Cards/PNG-cards-1.3/6_of_hearts.png"
+sixOfHearts.width = 200
+sixOfHearts.height = 300
+const sixOfSpades = new Image()
+sixOfSpades.src = "./Playing Cards/PNG-cards-1.3/6_of_spades.png"
+sixOfSpades.width = 200
+sixOfSpades.height = 300
+const sevenOfClubs = new Image()
+sevenOfClubs.src = "./Playing Cards/PNG-cards-1.3/7_of_clubs.png"
+sevenOfClubs.width = 200
+sevenOfClubs.height = 300
+const sevenOfDiamonds = new Image()
+sevenOfDiamonds.src = "./Playing Cards/PNG-cards-1.3/7_of_diamonds.png"
+sevenOfDiamonds.width = 200
+sevenOfDiamonds.height = 300
+const sevenOfHearts = new Image()
+sevenOfHearts.src = "./Playing Cards/PNG-cards-1.3/7_of_hearts.png"
+sevenOfHearts.width = 200
+sevenOfHearts.height = 300
+const sevenOfSpades = new Image()
+sevenOfSpades.src = "./Playing Cards/PNG-cards-1.3/7_of_spades.png"
+sevenOfSpades.width = 200
+sevenOfSpades.height = 300
+const eightOfClubs = new Image()
+eightOfClubs.src = "./Playing Cards/PNG-cards-1.3/8_of_clubs.png"
+eightOfClubs.width = 200
+eightOfClubs.height = 300
+const eightOfDiamonds = new Image()
+eightOfDiamonds.src = "./Playing Cards/PNG-cards-1.3/8_of_diamonds.png"
+eightOfDiamonds.width = 200
+eightOfDiamonds.height = 300
+const eightOfHearts = new Image()
+eightOfHearts.src = "./Playing Cards/PNG-cards-1.3/8_of_hearts.png"
+eightOfHearts.width = 200
+eightOfHearts.height = 300
+const eightOfSpades = new Image()
+eightOfSpades.src = "./Playing Cards/PNG-cards-1.3/8_of_spades.png"
+eightOfSpades.width = 200
+eightOfSpades.height = 300
+const nineOfClubs = new Image()
+nineOfClubs.src = "./Playing Cards/PNG-cards-1.3/9_of_clubs.png"
+nineOfClubs.width = 200
+nineOfClubs.height = 300
+const nineOfDiamonds = new Image()
+nineOfDiamonds.src = "./Playing Cards/PNG-cards-1.3/9_of_diamonds.png"
+nineOfDiamonds.width = 200
+nineOfDiamonds.height = 300
+const nineOfHearts = new Image()
+nineOfHearts.src = "./Playing Cards/PNG-cards-1.3/9_of_hearts.png"
+nineOfHearts.width = 200
+nineOfHearts.height = 300
+const nineOfSpades = new Image()
+nineOfSpades.src = "./Playing Cards/PNG-cards-1.3/9_of_spades.png"
+nineOfSpades.width = 200
+nineOfSpades.height = 300
+const tenOfClubs = new Image()
+tenOfClubs.src = "./Playing Cards/PNG-cards-1.3/10_of_clubs.png"
+tenOfClubs.width = 200
+tenOfClubs.height = 300
+const tenOfDiamonds = new Image()
+tenOfDiamonds.src = "./Playing Cards/PNG-cards-1.3/10_of_diamonds.png"
+tenOfDiamonds.width = 200
+tenOfDiamonds.height = 300
+const tenOfHearts = new Image()
+tenOfHearts.src = "./Playing Cards/PNG-cards-1.3/10_of_hearts.png"
+tenOfHearts.width = 200
+tenOfHearts.height = 300
+const tenOfSpades = new Image()
+tenOfSpades.src = "./Playing Cards/PNG-cards-1.3/10_of_spades.png"
+tenOfSpades.width = 200
+tenOfSpades.height = 300
+const aceOfClubs = new Image()
+aceOfClubs.src = "./Playing Cards/PNG-cards-1.3/ace_of_clubs.png"
+aceOfClubs.width = 200
+aceOfClubs.height = 300
+const aceOfDiamonds = new Image()
+aceOfDiamonds.src = "./Playing Cards/PNG-cards-1.3/ace_of_diamonds.png"
+aceOfDiamonds.width = 200
+aceOfDiamonds.height = 300
+const aceOfHearts = new Image()
+aceOfHearts.src = "./Playing Cards/PNG-cards-1.3/ace_of_hearts.png"
+aceOfHearts.width = 200
+aceOfHearts.height = 300
+const aceOfSpades = new Image()
+aceOfSpades.src = "./Playing Cards/PNG-cards-1.3/ace_of_spades.png"
+aceOfSpades.width = 200
+aceOfSpades.height = 300
+const jackOfClubs = new Image()
+jackOfClubs.src = "./Playing Cards/PNG-cards-1.3/jack_of_clubs.png"
+jackOfClubs.width = 200
+jackOfClubs.height = 300
+const jackOfDiamonds = new Image()
+jackOfDiamonds.src = "./Playing Cards/PNG-cards-1.3/jack_of_diamonds.png"
+jackOfDiamonds.width = 200
+jackOfDiamonds.height = 300
+const jackOfHearts = new Image()
+jackOfHearts.src = "./Playing Cards/PNG-cards-1.3/jack_of_hearts.png"
+jackOfHearts.width = 200
+jackOfHearts.height = 300
+const jackOfSpades = new Image()
+jackOfSpades.src = "./Playing Cards/PNG-cards-1.3/jack_of_spades.png"
+jackOfSpades.width = 200
+jackOfSpades.height = 300
+const kingOfClubs = new Image()
+kingOfClubs.src = "./Playing Cards/PNG-cards-1.3/king_of_clubs.png"
+kingOfClubs.width = 200
+kingOfClubs.height = 300
+const kingOfDiamonds = new Image()
+kingOfDiamonds.src = "./Playing Cards/PNG-cards-1.3/king_of_diamonds.png"
+kingOfDiamonds.width = 200
+kingOfDiamonds.height = 300
+const kingOfHearts = new Image()
+kingOfHearts.src = "./Playing Cards/PNG-cards-1.3/king_of_hearts.png"
+kingOfHearts.width = 200
+kingOfHearts.height = 300
+const kingOfSpades = new Image()
+kingOfSpades.src = "./Playing Cards/PNG-cards-1.3/king_of_spades.png"
+kingOfSpades.width = 200
+kingOfSpades.height = 300
+const queenOfClubs = new Image()
+queenOfClubs.src = "./Playing Cards/PNG-cards-1.3/queen_of_clubs.png"
+queenOfClubs.width = 200
+queenOfClubs.height = 300
+const queenOfDiamonds = new Image()
+queenOfDiamonds.src = "./Playing Cards/PNG-cards-1.3/queen_of_diamonds.png"
+queenOfDiamonds.width = 200
+queenOfDiamonds.height = 300
+const queenOfHearts = new Image()
+queenOfHearts.src = "./Playing Cards/PNG-cards-1.3/queen_of_hearts.png"
+queenOfHearts.width = 200
+queenOfHearts.height = 300
+const queenOfSpades = new Image()
+queenOfSpades.src = "./Playing Cards/PNG-cards-1.3/queen_of_spades.png"
+queenOfSpades.width = 200
+queenOfSpades.height = 300
+const redJoker = new Image()
+redJoker.src = "./Playing Cards/PNG-cards-1.3/red_joker.png"
+redJoker.width = 200
+redJoker.height = 300
+const blackJoker = new Image()
+blackJoker.src = "./Playing Cards/PNG-cards-1.3/black_joker.png"
+blackJoker.width = 200
+blackJoker.height = 300
+
+const playingCards = []
+const dealBtn = document.querySelector("#deal");
+dealerHand = document.getElementById("dealer-hand");
+playerHand = document.getElementById("player-hand");
+
+
+playingCards.push(twoOfClubs, twoOfDiamonds, twoOfHearts, twoOfSpades,
+     threeOfClubs, threeOfDiamonds, threeOfHearts, threeOfSpades, 
+     fourOfClubs, fourOfDiamonds, fourOfHearts, fourOfSpades, 
+     fiveOfClubs, fiveOfDiamonds, fiveOfHearts, fiveOfSpades, 
+     sixOfClubs, sixOfDiamonds, sixOfHearts, sixOfSpades,
+     sevenOfClubs, sevenOfDiamonds, sevenOfHearts, sevenOfSpades, 
+     eightOfClubs, eightOfDiamonds, eightOfHearts, eightOfSpades,
+     nineOfHearts, nineOfClubs, nineOfDiamonds, nineOfSpades,
+     tenOfClubs, tenOfDiamonds, tenOfHearts, tenOfSpades,
+     aceOfClubs, aceOfDiamonds, aceOfHearts, aceOfSpades,
+     kingOfClubs, kingOfDiamonds, kingOfHearts, kingOfSpades,
+     queenOfClubs, queenOfDiamonds, queenOfHearts, queenOfSpades, 
+     jackOfClubs, jackOfDiamonds, jackOfHearts, jackOfSpades);
+
+const twos = {
+     cards: [twoOfClubs, twoOfDiamonds, twoOfHearts, twoOfSpades],
+     value: 2
+};
+const threes = {
+     cards: [threeOfClubs, threeOfDiamonds, threeOfHearts, threeOfSpades],
+     value: 3
+};
+const fours = {
+     cards: [fourOfClubs, fourOfDiamonds, fourOfHearts, fourOfSpades],
+     value: 4
+};
+const fives = {
+     cards: [fiveOfClubs, fiveOfDiamonds, fiveOfHearts, fiveOfSpades],
+     value: 5
+};
+const sixes = {
+     cards: [sixOfClubs, sixOfDiamonds, sixOfHearts, sixOfSpades],
+     value: 6
+};
+const sevens = {
+     cards: [sevenOfClubs, sevenOfDiamonds, sevenOfHearts, sevenOfSpades],
+     value: 7
+};
+const eights = {
+     cards: [eightOfClubs, eightOfDiamonds, eightOfHearts, eightOfSpades],
+     value: 8
+};
+const nines = {
+     cards: [nineOfHearts, nineOfClubs, nineOfDiamonds, nineOfSpades],
+     value: 9
+};
+const tens = {
+     cards: [tenOfClubs, tenOfDiamonds, tenOfHearts, tenOfSpades],
+     value: 10
+};
+const kings = {
+     cards: [kingOfClubs, kingOfDiamonds, kingOfHearts, kingOfSpades],
+     value: 10
+};
+const queens = {
+     cards: [queenOfClubs, queenOfDiamonds, queenOfHearts, queenOfSpades],
+     value: 10
+};
+const jacks = {
+     cards: [jackOfClubs, jackOfDiamonds, jackOfHearts, jackOfSpades],
+     value: 10
+};
+const aces = {
+     cards: [aceOfClubs, aceOfDiamonds, aceOfHearts, aceOfSpades],
+     value: 11
+};
+
+function deal() {
+     let r = Math.floor(Math.random()*playingCards.length);
+     let card = playingCards[r];
+     playerHand.appendChild(card);
+}
+dealBtn.addEventListener("click", deal);
+
+const aceNames = aces.cards.forEach(card => card.name = "ace")
+
+console.log(aceOfClubs.name);
+console.log(twoOfDiamonds.name);
---
+++ script.js
@@ -0,0 +1,268 @@
+const twoOfClubs = new Image()
+twoOfClubs.src = "./Playing Cards/PNG-cards-1.3/2_of_clubs.png"
+const twoOfDiamonds = new Image()
+twoOfDiamonds.src = "./Playing Cards/PNG-cards-1.3/2_of_diamonds.png"
+const twoOfHearts = new Image()
+twoOfHearts.src = "./Playing Cards/PNG-cards-1.3/2_of_hearts.png"
+const twoOfSpades = new Image()
+twoOfSpades.src = "./Playing Cards/PNG-cards-1.3/2_of_spades.png"
+const threeOfClubs = new Image()
+threeOfClubs.src = "./Playing Cards/PNG-cards-1.3/3_of_clubs.png"
+const threeOfDiamonds = new Image()
+threeOfDiamonds.src = "./Playing Cards/PNG-cards-1.3/3_of_diamonds.png"
+const threeOfHearts = new Image()
+threeOfHearts.src = "./Playing Cards/PNG-cards-1.3/3_of_hearts.png"
+const threeOfSpades = new Image()
+threeOfSpades.src = "./Playing Cards/PNG-cards-1.3/3_of_spades.png"
+const fourOfClubs = new Image()
+fourOfClubs.src = "./Playing Cards/PNG-cards-1.3/4_of_clubs.png"
+const fourOfDiamonds = new Image()
+fourOfDiamonds.src = "./Playing Cards/PNG-cards-1.3/4_of_diamonds.png"
+const fourOfHearts = new Image()
+fourOfHearts.src = "./Playing Cards/PNG-cards-1.3/4_of_hearts.png"
+const fourOfSpades = new Image()
+fourOfSpades.src = "./Playing Cards/PNG-cards-1.3/4_of_spades.png"
+const fiveOfClubs = new Image()
+fiveOfClubs.src = "./Playing Cards/PNG-cards-1.3/5_of_clubs.png"
+const fiveOfDiamonds = new Image()
+fiveOfDiamonds.src = "./Playing Cards/PNG-cards-1.3/5_of_diamonds.png"
+const fiveOfHearts = new Image()
+fiveOfHearts.src = "./Playing Cards/PNG-cards-1.3/5_of_hearts.png"
+const fiveOfSpades = new Image()
+fiveOfSpades.src = "./Playing Cards/PNG-cards-1.3/5_of_spades.png"
+const sixOfClubs = new Image()
+sixOfClubs.src = "./Playing Cards/PNG-cards-1.3/6_of_clubs.png"
+const sixOfDiamonds = new Image()
+sixOfDiamonds.src = "./Playing Cards/PNG-cards-1.3/6_of_diamonds.png"
+const sixOfHearts = new Image()
+sixOfHearts.src = "./Playing Cards/PNG-cards-1.3/6_of_hearts.png"
+const sixOfSpades = new Image()
+sixOfSpades.src = "./Playing Cards/PNG-cards-1.3/6_of_spades.png"
+const sevenOfClubs = new Image()
+sevenOfClubs.src = "./Playing Cards/PNG-cards-1.3/7_of_clubs.png"
+const sevenOfDiamonds = new Image()
+sevenOfDiamonds.src = "./Playing Cards/PNG-cards-1.3/7_of_diamonds.png"
+const sevenOfHearts = new Image()
+sevenOfHearts.src = "./Playing Cards/PNG-cards-1.3/7_of_hearts.png"
+const sevenOfSpades = new Image()
+sevenOfSpades.src = "./Playing Cards/PNG-cards-1.3/7_of_spades.png"
+const eightOfClubs = new Image()
+eightOfClubs.src = "./Playing Cards/PNG-cards-1.3/8_of_clubs.png"
+const eightOfDiamonds = new Image()
+eightOfDiamonds.src = "./Playing Cards/PNG-cards-1.3/8_of_diamonds.png"
+const eightOfHearts = new Image()
+eightOfHearts.src = "./Playing Cards/PNG-cards-1.3/8_of_hearts.png"
+const eightOfSpades = new Image()
+eightOfSpades.src = "./Playing Cards/PNG-cards-1.3/8_of_spades.png"
+const nineOfClubs = new Image()
+nineOfClubs.src = "./Playing Cards/PNG-cards-1.3/9_of_clubs.png"
+const nineOfDiamonds = new Image()
+nineOfDiamonds.src = "./Playing Cards/PNG-cards-1.3/9_of_diamonds.png"
+const nineOfHearts = new Image()
+nineOfHearts.src = "./Playing Cards/PNG-cards-1.3/9_of_hearts.png"
+const nineOfSpades = new Image()
+nineOfSpades.src = "./Playing Cards/PNG-cards-1.3/9_of_spades.png"
+const tenOfClubs = new Image()
+tenOfClubs.src = "./Playing Cards/PNG-cards-1.3/10_of_clubs.png"
+const tenOfDiamonds = new Image()
+tenOfDiamonds.src = "./Playing Cards/PNG-cards-1.3/10_of_diamonds.png"
+const tenOfHearts = new Image()
+tenOfHearts.src = "./Playing Cards/PNG-cards-1.3/10_of_hearts.png"
+const tenOfSpades = new Image()
+tenOfSpades.src = "./Playing Cards/PNG-cards-1.3/10_of_spades.png"
+const aceOfClubs = new Image()
+aceOfClubs.src = "./Playing Cards/PNG-cards-1.3/ace_of_clubs.png"
+const aceOfDiamonds = new Image()
+aceOfDiamonds.src = "./Playing Cards/PNG-cards-1.3/ace_of_diamonds.png"
+const aceOfHearts = new Image()
+aceOfHearts.src = "./Playing Cards/PNG-cards-1.3/ace_of_hearts.png"
+const aceOfSpades = new Image()
+aceOfSpades.src = "./Playing Cards/PNG-cards-1.3/ace_of_spades.png"
+const jackOfClubs = new Image()
+jackOfClubs.src = "./Playing Cards/PNG-cards-1.3/jack_of_clubs.png"
+const jackOfDiamonds = new Image()
+jackOfDiamonds.src = "./Playing Cards/PNG-cards-1.3/jack_of_diamonds.png"
+const jackOfHearts = new Image()
+jackOfHearts.src = "./Playing Cards/PNG-cards-1.3/jack_of_hearts.png"
+const jackOfSpades = new Image()
+jackOfSpades.src = "./Playing Cards/PNG-cards-1.3/jack_of_spades.png"
+const kingOfClubs = new Image()
+kingOfClubs.src = "./Playing Cards/PNG-cards-1.3/king_of_clubs.png"
+const kingOfDiamonds = new Image()
+kingOfDiamonds.src = "./Playing Cards/PNG-cards-1.3/king_of_diamonds.png"
+const kingOfHearts = new Image()
+kingOfHearts.src = "./Playing Cards/PNG-cards-1.3/king_of_hearts.png"
+const kingOfSpades = new Image()
+kingOfSpades.src = "./Playing Cards/PNG-cards-1.3/king_of_spades.png"
+const queenOfClubs = new Image()
+queenOfClubs.src = "./Playing Cards/PNG-cards-1.3/queen_of_clubs.png"
+const queenOfDiamonds = new Image()
+queenOfDiamonds.src = "./Playing Cards/PNG-cards-1.3/queen_of_diamonds.png"
+const queenOfHearts = new Image()
+queenOfHearts.src = "./Playing Cards/PNG-cards-1.3/queen_of_hearts.png"
+const queenOfSpades = new Image()
+queenOfSpades.src = "./Playing Cards/PNG-cards-1.3/queen_of_spades.png"
+const redJoker = new Image()
+redJoker.src = "./Playing Cards/PNG-cards-1.3/red_joker.png"
+const blackJoker = new Image()
+blackJoker.src = "./Playing Cards/PNG-cards-1.3/black_joker.png"
+
+const message = document.querySelector("#notification");
+const buttonsContainer = document.querySelector('#buttons');
+const dealBtn = document.querySelector("#deal");
+const playerHand = document.getElementById("player-hand");
+const startBtn = document.querySelector("#start");
+dealBtn.hidden = true;
+let handValue = 0;
+let isAlive = false;
+let hasBlackjack = false;
+
+const twos = {
+     cards: [twoOfClubs, twoOfDiamonds, twoOfHearts, twoOfSpades],
+     value: 2
+};
+const threes = {
+     cards: [threeOfClubs, threeOfDiamonds, threeOfHearts, threeOfSpades],
+     value: 3
+};
+const fours = {
+     cards: [fourOfClubs, fourOfDiamonds, fourOfHearts, fourOfSpades],
+     value: 4
+};
+const fives = {
+     cards: [fiveOfClubs, fiveOfDiamonds, fiveOfHearts, fiveOfSpades],
+     value: 5
+};
+const sixes = {
+     cards: [sixOfClubs, sixOfDiamonds, sixOfHearts, sixOfSpades],
+     value: 6
+};
+const sevens = {
+     cards: [sevenOfClubs, sevenOfDiamonds, sevenOfHearts, sevenOfSpades],
+     value: 7
+};
+const eights = {
+     cards: [eightOfClubs, eightOfDiamonds, eightOfHearts, eightOfSpades],
+     value: 8
+};
+const nines = {
+     cards: [nineOfHearts, nineOfClubs, nineOfDiamonds, nineOfSpades],
+     value: 9
+};
+const tens = {
+     cards: [tenOfClubs, tenOfDiamonds, tenOfHearts, tenOfSpades],
+     value: 10
+};
+const kings = {
+     cards: [kingOfClubs, kingOfDiamonds, kingOfHearts, kingOfSpades],
+     value: 10
+};
+const queens = {
+     cards: [queenOfClubs, queenOfDiamonds, queenOfHearts, queenOfSpades],
+     value: 10
+};
+const jacks = {
+     cards: [jackOfClubs, jackOfDiamonds, jackOfHearts, jackOfSpades],
+     value: 10
+};
+const aces = {
+     cards: [aceOfClubs, aceOfDiamonds, aceOfHearts, aceOfSpades],
+     value: 11
+};
+
+const allCards = [...twos.cards, ...threes.cards, ...fours.cards, 
+     ...fives.cards, ...sixes.cards, ...sevens.cards, ...eights.cards, 
+     ...nines.cards, ...tens.cards, ...kings.cards, ...queens.cards,
+     ...jacks.cards, ...aces.cards
+]
+const cardHeights = allCards.forEach(function (card) {
+     card.height = 100;
+     card.width = 62
+})
+
+const acesNames = aces.cards.forEach(card => card.name = "ace");
+const kingsNames = kings.cards.forEach(card => card.name = "king");
+const jacksNames = jacks.cards.forEach(card => card.name = "jack");
+const queensNames = queens.cards.forEach(card => card.name = "queen");
+const twosNames = twos.cards.forEach(card => card.name = "two");
+const threesNames = threes.cards.forEach(card => card.name = "three");
+const foursNames = fours.cards.forEach(card => card.name = "four");
+const fivesNames = fives.cards.forEach(card => card.name = "five");
+const sixesNames = sixes.cards.forEach(card => card.name = "six");
+const sevensNames = sevens.cards.forEach(card => card.name = "seven");
+const eightsNames = eights.cards.forEach(card => card.name = "eight");
+const ninesNames = nines.cards.forEach(card => card.name = "nine");
+const tensNames = tens.cards.forEach(card => card.name = "ten");
+
+function startGame() {
+     startBtn.hidden = true;
+     dealBtn.hidden = false;
+     handValue = 0;
+     isAlive = true;
+     hasBlackjack = false;
+     playerHand.innerHTML = "";
+     message.textContent = "Game started! Draw your cards.";
+};
+
+function randomCard() {
+     let r = Math.floor(Math.random()*allCards.length);
+     let card = allCards[r];
+     return card;
+}
+
+function assignCardValue(card) {
+     if (card.name === "two") return twos.value;
+     if (card.name === "three") return threes.value;
+     if (card.name === "four") return fours.value;
+     if (card.name === "five") return fives.value;
+     if (card.name === "six") return sixes.value;
+     if (card.name === "seven") return sevens.value;
+     if (card.name === "eight") return eights.value;
+     if (card.name === "nine") return nines.value;
+     if (card.name === "ten") return tens.value;
+     if (card.name === "ace") return aces.value;
+     if (card.name === "king") return kings.value;
+     if (card.name === "queen") return queens.value;
+     if (card.name === "jack") return jacks.value;
+     return 0;
+ }; 
+
+const restartGameBtn = document.createElement('button');
+restartGameBtn.textContent = 'NEW GAME';
+restartGameBtn.id = 'new-game';         
+restartGameBtn.className = 'Restart-game';
+
+function renderGame() {
+     const card = randomCard();
+     const value = assignCardValue(card);
+     playerHand.appendChild(card);
+     if (isAlive === true) {
+          if(handValue <= 21) {
+             handValue += value;
+             if (handValue < 21) {
+               message.innerHTML = `Draw another card? <br> Current hand value: ${handValue}`;
+               isAlive = true;
+               hasBlackjack = false;
+          } else if (handValue > 21) {
+               isAlive = false;
+               hasBlackjack = false;
+               message.innerHTML = `Bust!! Better Luck Next Time <br> Current hand value: ${handValue}`;
+               dealBtn.removeEventListener("click", renderGame);
+               dealBtn.hidden = true;
+               buttonsContainer.appendChild(restartGameBtn);
+          } else {
+               isAlive = false;
+               hasBlackjack = true;
+               message.textContent = message.innerHTML = `Congratulations!! You Have Black Jack!! <br> Current hand value: ${handValue}`;; 
+               dealBtn.removeEventListener("click", renderGame);
+               dealBtn.hidden = true;
+               buttonsContainer.appendChild(restartGameBtn);
+          }  
+          }
+     }
+console.log(isAlive, handValue);
+
+
+dealBtn.addEventListener("click", renderGame);
+startBtn.addEventListener('click', startGame);
+restartGameBtn.addEventListener('click', () => location.reload());
